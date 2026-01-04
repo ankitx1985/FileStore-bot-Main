a44e69ad9f0964c158be0ca495880a7a34bf1ccc
ankitx1985 <ankitsharma20045@gmail.com>
2026-01-04 23:00:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -24,7 +24,7 @@ DB_NAME = "heisenberg"
 
 FSUBS = [[-1003032346949, False, 10]] # Force Subscription Channels [channel_id, request_enabled, timer_in_minutes]
 # Database Channel (Primary)
-DB_CHANNEL =    # just put channel id dont add ""
+DB_CHANNEL =  -1002936339662  # just put channel id dont add ""
 # Multiple Database Channels (can be set via bot settings)
 # DB_CHANNELS = {
 #     "1002936339662": {"name": "Primary DB", "is_primary": True, "is_active": True},
